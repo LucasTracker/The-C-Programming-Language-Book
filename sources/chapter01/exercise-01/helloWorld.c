8fe048180771ca660fcdb04452b4d86ec5689ff5
Lucas Tracker <lucasgabriel3490@gmail.com>
2022-11-07 21:15:38 -0300

    helloWorld updated

--- sources/chapter01/exercise-01/helloWorld.c
+++ sources/chapter01/exercise-01/helloWorld.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+//  Run the "hello,world" program on your system. Experiment
+// with leaving out parts of the program, to see what error messages you get
+
 int main(void){
 
 	printf("Hello, World\n");
