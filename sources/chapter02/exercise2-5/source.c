6ce8fb04bd1847a34dafeb0abf8e8bc5d82d8bc6
LucasTracker <lucasgabriel3490@gmail.com>
2023-02-20 20:35:14 -0300

    Exercise 2.5

---
+++ sources/chapter02/exercise2-5/source.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+/*
+Write the function any(s1,s2), which returns the first location in a string s1
+where any character from the string s2 occurs, or -1 if s1 contains no characters from s2.
+
+*/
+
+
+int any(char*,char*);
+
+int main(void){
+	printf("%d\n",any("lucas","peace"));
+	return 0;
+}
+
+int any(char* s1, char* s2){
+	int i,j;
+
+	for(i = 0; *(s1 + i) != '\0'; i++){
+		for(j = 0; *(s2+j) != '\0'; j++){
+			if(*(s1 + i) == *(s2 + j)) return i+1;
+		}
+	}
+
+	return -1;
+}
